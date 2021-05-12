a04c0c50ffd0e21d50d6b263ec4a42b5f86d2a8c
Sohee <sohee3480@gmail.com>
2021-05-12 21:20:33 +0900

    어댑터 연결해서 내용 반영하기

--- app/src/main/java/com/example/landapp_sohee/adapters/RoomAdapter.kt
+++ app/src/main/java/com/example/landapp_sohee/adapters/RoomAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.example.landapp_sohee.R
 import com.example.landapp_sohee.datas.Room
 
@@ -27,6 +28,23 @@ class RoomAdapter(val mContext: Context,
         }
 
         val row = tempRow!!
+
+
+        val roomData = mList[position]
+
+
+        val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
+
+        val addressAndFloorTxt = row.findViewById<TextView>(R.id.addressAndFloorTxt)
+
+        val descriptionTxt = row.findViewById<TextView>(R.id.descriptionTxt)
+
+
+
+        descriptionTxt.text = roomData.description
+
+        addressAndFloorTxt.text = roomData.address
+
 
 
         return row
